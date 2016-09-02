21d02c779552675bee0ba17b7530c966ff412af0
Jonathan <jkoomjian@gmail.com>
2016-09-02 16:06:18 -0700

    using web animations api to randomize the falling rate

--- Confetti/main.js
+++ Confetti/main.js
@@ -1,9 +1,3 @@
-//+color
-//+fall
-//+rotate
-//randomize the fall rate, size?
-//autoprefix
-
 $ = document.querySelector.bind(document);
 $$ = document.querySelectorAll.bind(document);
 
@@ -11,6 +5,16 @@ var confetti = (function() {
 
   var hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
   var starCount = 0;
+  var animationSettings = {
+    duration: 5000,
+    easing: 'linear',
+    iterations: Infinity,
+  };
+  var animationKeyFrames = [
+    {transform: 'rotate(0deg) rotateY(0deg) rotateX(0deg)', top: '0%'},
+    {transform: 'rotate(360deg) rotateY(360deg) rotateX(360deg)', top: '130%'}
+  ];
+
 
   //returns a random number between low and high, inclusive
   function randomRange(low, high) {
@@ -41,6 +45,13 @@ var confetti = (function() {
       $("body").appendChild(star);
       star.style.left = randomRange(0, getViewport().w - star.offsetWidth) + "px";
       star.style.backgroundColor = randomColor();
+
+      //clone keyFrames
+      var custKeyFrames = animationKeyFrames.map( e => {return e;});
+      custKeyFrames[1].transform = `rotate(${randomRange(90,360)}deg) rotateY(${randomRange(90,360)}deg) rotateX(${randomRange(90,360)}deg)`;
+      // console.log(`transform settings: ${custKeyFrames[1].transform}`);
+      animationSettings.duration = randomRange(5000, 10000);
+      star.animate(custKeyFrames, animationSettings);
     },
     beginCreatingStars: function() {
       if (starCount > 50) return;
