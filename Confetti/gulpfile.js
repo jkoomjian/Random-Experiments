d1e00a405f42919eff31eb658cba007fa8e21422
Jonathan <jkoomjian@gmail.com>
2016-09-02 15:29:08 -0700

    adding autoprefixer

--- Confetti/gulpfile.js
+++ Confetti/gulpfile.js
@@ -2,10 +2,15 @@
 
 const gulp = require("gulp");
 const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', () => {
   gulp.src('style.scss')
       .pipe(sass())
+      .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
       .pipe(gulp.dest('.'));
 });
 
