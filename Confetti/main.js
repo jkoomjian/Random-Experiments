a23190a201e022deb02026a73597ce4513dc111e
Jonathan <jkoomjian@gmail.com>
2016-09-02 15:16:05 -0700

    performance improvements

--- Confetti/main.js
+++ Confetti/main.js
@@ -2,27 +2,29 @@
 //+fall
 //+rotate
 //randomize the fall rate, size?
+//autoprefix
 
 $ = document.querySelector.bind(document);
 $$ = document.querySelectorAll.bind(document);
 
 var confetti = (function() {
 
-  this.hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+  var hex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'];
+  var starCount = 0;
 
   //returns a random number between low and high, inclusive
-  this.randomRange = function(low, high) {
+  function randomRange(low, high) {
     return Math.floor( (Math.random() * (high + 1 - low)) + low );
   }
 
   //returns a random color in hex format
-  this.randomColor = function() {
+  function randomColor() {
     var color = "#";
     for (let i=0; i<6; i++) color += hex[ randomRange(0, 15) ];
     return color;
   }
 
-  this.getViewport = function() {
+  function getViewport() {
       return {w: $("body").offsetWidth, h: $("body").offsetHeight};
   }
 
@@ -41,8 +43,11 @@ var confetti = (function() {
       star.style.backgroundColor = randomColor();
     },
     beginCreatingStars: function() {
+      if (starCount > 50) return;
+      console.log(`added ${starCount} stars!`);
+      starCount++;
       this.generateStar();
-      window.setTimeout("confetti.beginCreatingStars()", randomRange(0, 1000));
+      window.setTimeout(this.beginCreatingStars.bind(this), randomRange(0, 1000));
     }
   };
 })();
