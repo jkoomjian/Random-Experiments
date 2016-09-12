e73037f7acf340f7c3870ad78b6400824aed9e68
Jonathan <jkoomjian@gmail.com>
2016-09-12 12:31:49 -0700

    moving legos project to its own repo

--- Legos/start.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-  var legoSpace = new LegoSpace();
-  var xPlane = new XPlane(legoSpace);
-  var yPlane = new YPlane(legoSpace);
-  var zPlane = new ZPlane(legoSpace);
-  initEventHandlers();
-});
