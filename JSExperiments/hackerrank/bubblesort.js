af2fdda5d6c822ed2a4e19898801f61f0ac4abb1
Jonathan <jkoomjian@gmail.com>
2016-10-29 16:39:24 -0700

    interview prep

--- JSExperiments/hackerrank/bubblesort.js
+++ JSExperiments/hackerrank/bubblesort.js
@@ -2,7 +2,6 @@ function bubbleSort(str) {
   var strA = str.split('');
 
   for (let i=0; i<str.length; i++) {
-// debugger
     for (let j=0; j<strA.length-1; j++) {
       let first = strA[j];
       let last = strA[j+1];
