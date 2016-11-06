e8189c28ad51296699d6945198f5923c9e9a860b
Jonathan <jkoomjian@gmail.com>
2016-11-06 09:16:03 -0800

    switch from directives to controllers

--- AngularProject2/src/stoplight-directives.js
+++ AngularProject2/src/stoplight-directives.js
@@ -3,42 +3,37 @@ const mod = angular.module('stopLightDirectivesModule', []);
 /*----------- Directives ----------------*/
 // change colors based on stopLightService.
 // uses attribute to determine which direction the stop light will use.
-mod.directive('stopLightRoad', function($templateCache, stopLightService){
-  return {
-    restrict: 'E',
-    scope: true,
-    templateUrl: 'templates/stoplight.html',
-    link: function(scope, elem, attrs) {
-      scope.lightDirection = attrs["lightDirection"];
-      var streetDirection = attrs["streetDirection"];
-      scope.streetDirection = streetDirection;
-      scope.showLight = function(color) {
-        return stopLightService.isLit(streetDirection, color) ? "on" : "";
-      };
+mod.component('stopLightRoad', {
+  templateUrl: 'templates/stoplight.html',
+  bindings: {
+    lightDirection:  '@',
+    streetDirection: '@'
+  },
+  //controller, must be wrapped in a function to get access to this
+  controller: function(stopLightService) {
+    this.showLight = function(color) {
+      return stopLightService.isLit(this.streetDirection, color) ? "on" : "";
     }
-  };
+  }
 });
 
-mod.directive('lightModeButton', function(stopLightService) {
-  return {
-    restrict: 'E',
-    replace: true,  // replace the directive element
-    scope: true,  // create a new child scope
-    templateUrl: "templates/lightmodebutton.html",
-    link: function(scope, elem, attrs) {
-      let idText = {manual: "toggleLights", auto: "autoLights"};
-      let mode = attrs['lightMode'];
-      scope.buttonText = attrs['buttonText'];
-      scope.buttonId = idText[mode];
-      scope.isInMode = function() {
-        return stopLightService.getMode() == mode;
-      };
-      scope.setMode  = function() {
-        stopLightService.setMode(mode);
-        if (mode == 'manual') stopLightService.toggleLights();
-        if (mode == 'auto') stopLightService.startAutoMode();
-      };
-    }
+mod.component('lightModeButton', {
+  templateUrl: "templates/lightmodebutton.html",
+  bindings: {
+    mode: '@lightMode',
+    buttonText: '@'
+  },
+  controller: function(stopLightService) {
+    let idText = {manual: "toggleLights", auto: "autoLights"};
+    this.buttonId = idText[this.mode];
+    this.isInMode = function() {
+      return stopLightService.getMode() == this.mode;
+    };
+    this.setMode  = function() {
+      stopLightService.setMode(this.mode);
+      if (this.mode == 'manual') stopLightService.toggleLights();
+      if (this.mode == 'auto') stopLightService.startAutoMode();
+    };
   }
 });
 
