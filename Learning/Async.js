7d65614236d7ea58bdefb5bd90c7b2a1229f3d62
Jonathan <jkoomjian@gmail.com>
2017-05-17 23:18:24 -0700

    adding async

---
+++ Learning/Async.js
@@ -0,0 +1,69 @@
+/**
+ * Async is an improvement over promises.
+ * Available in Node 7.7+
+ * Works similarly to generators, suspending execution in your context until the promise settles.
+ * If the awaited expression isn’t a promise, its casted into a promise.
+ */
+
+const getJson = () {my: 'json'};
+
+ /* Promises version */
+ const makeRequest = () =>
+  getJSON()
+    .then(data => {
+      console.log(data)
+      return "done"
+    })
+makeRequest()
+
+/* Async Version */
+const makeRequest = async function() {   // prefixing function with 'async' makes it asynchronous
+  console.log(await getJSON())              // blocks here until getJson() returns
+  return "done"
+}
+makeRequest()
+
+// To use async, the function must have async in front of it. This makes the whole function asynchrounous.
+// Then use await where the actual asynchrounous code is called
+// Any function called by
+
+function myAsyncMethod() {
+  return new Promise(function(resolve, reject) {
+    //your async code goes here
+    setTimeout(function() {
+      resolve("success");
+    }, 3000);
+  });
+}
+
+let makeRequest = async () => {
+  console.log('making async request');
+  let myPromiseResult = await myAsyncMethod();
+  console.log('myAsyncMethod returned', myPromiseResult);
+  console.log('returning done');
+  // Never return anything from an async function - it will automatically return a Promise
+  return myPromiseResult + "-done";
+}
+console.log('makeRequest returned: ', makeRequest());
+
+// > console.log('makeRequest returned: ', makeRequest());
+// making async request
+// makeRequest returned:  Promise { <pending> }
+// undefined
+// > myAsyncMethod returned success
+// returning done
+
+// makeRequest pauses at myAsyncMethod() while the promise resolves
+// but makeRequest is an async method - so it doesn't block (returning something doesn't really make sense)
+// An Async Function always returns a Promise
+
+// Instead use Promise async functions return
+makeRequest().then( (x) => console.log('makeRequest returned: ', x) );
+// makeRequest returned:  success-done
+
+// While Async Functions make it easier to write asynchronous code, they also lend themselves to code that is serial.
+// That is to say: code that executes one operation at a time. A function with multiple await expressions in it will be
+// suspended once at a time on each await expression until that Promise is settled
+
+// Overall, seems complicated to balance sync, async code, still requires you to know promises.
+// Also poor support. Not worth using at the moment.
